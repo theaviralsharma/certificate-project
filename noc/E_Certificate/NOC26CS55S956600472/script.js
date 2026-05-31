9206553a592a71f0d1e5fd9dbdbfe378135f3602
Aviral Sharma <beingaviralsharma@gmail.com>
2026-05-31 14:26:14 +0530

    fix

--- noc/E_Certificate/NOC26CS55S956600472/script.js
+++ noc/E_Certificate/NOC26CS55S956600472/script.js
@@ -1,17 +1,14 @@
 const pdfURL = "NOC26CS55S956600472.pdf";
 
-function openPDF() {
-  window.location.href = pdfURL;
+function openPDF(){
+    window.location.href = pdfURL;
 }
 
 const ua = navigator.userAgent;
 
 const isAndroid = /Android/i.test(ua);
 const isIphone = /iPhone/i.test(ua);
-const isIpad =
-  /iPad/i.test(ua) ||
-  (navigator.platform === "MacIntel" && navigator.maxTouchPoints > 1);
-
+const isIpad = /iPad/i.test(ua);
 const isMac = /Macintosh|Mac OS X/i.test(ua);
 const isWindows = /Windows/i.test(ua);
 
@@ -19,14 +16,25 @@ const androidView = document.getElementById("androidView");
 const iphoneView = document.getElementById("iphoneView");
 const desktopView = document.getElementById("desktopView");
 
-if (isAndroid) {
-  androidView.style.display = "flex";
-} else if (isIphone || isIpad) {
-  iphoneView.style.display = "flex";
-} else if (isMac || isWindows) {
-  desktopView.style.display = "flex";
-  window.location.href = pdfURL;
-} else {
-  desktopView.style.display = "flex";
-  window.location.href = pdfURL;
+if(isAndroid){
+
+    androidView.style.display = "flex";
+
+}
+else if(isIphone || isIpad){
+
+    iphoneView.style.display = "flex";
+
+}
+else if(isMac || isWindows){
+
+    desktopView.style.display = "flex";
+    window.location.href = pdfURL;
+
+}
+else{
+
+    desktopView.style.display = "flex";
+    window.location.href = pdfURL;
+
 }
